b7650bd1c2965373bcaa9be29c20d11129452beb
Cramer <cramerm@nih.gov>
2020-03-06 20:18:52 -0500

    Took out text that is inserted by JS

--- script.js
+++ script.js
@@ -24,8 +24,47 @@ function onSpeak(e) {
   // console.log(e);
   const msg = e.results[0][0].transcript;
   // console.log(msg);
-  // writeMessage(msg);
-  // checkNumber(msg);
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+// Write what user  speaks
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class="box">${msg}</span>
+  `;
+}
+
+// Check message agains number
+// convert message to a number, with the + and asisgn to num
+function checkNumber(msg) {
+  const num = +msg;
+
+  // check if valid number
+  if(Number.isNaN(num)) {
+    msgEl.innerHTML += `<div>This is not a valid number.</div>`;
+    return;
+  }
+
+  // check if number is in range
+  if(num > 100 || num < 1) {
+    msgEl.innerHTML += `<div>Number must be  between 1 and 100</div>`;
+    return;
+  }
+
+  // check number
+  if(num === randomNum) {
+    document.body.innerHTML += `
+      <h2>Contrats! You have guessed the number!<br><br>
+      It was ${num};</h2>
+      <button class="play-again" id="play-again">Play Again</button>
+    `;
+  } else if (num < randomNum) {
+    msgEl.innerHTML += `<div>GO HIGHER</div>`;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += `<div>GO LOWER</div>`;
+  }
 }
 
 // get a random number 1 through 99
@@ -35,3 +74,13 @@ function getRandomNumber() {
 
 // Speak result, when the recognition object gets a result, fire  onSpeak() function
 recognition.addEventListener('result', onSpeak);
+
+// End SR service
+recognition.addEventListener('end', () => recognition.start());
+
+// Start game over
+document.body.addEventListener('click', (e) => {
+  if(e.target.id == 'play-again') {
+    window.location.reload();
+  }
+});
